1a004b3530b546ff3cf6d4e17e3dc7b2a33cfc58
Admin <nicholasfran45@gmail.com>
2023-02-03 15:58:43 -0500

    Added to recipe instructions class

--- Recipe_Ingrediants.py
+++ Recipe_Ingrediants.py
@@ -3,10 +3,12 @@ class RecipeIngrediants:
     def __init__(self):
         self.ingrediant_row = []
 
+    # iterate over ingrediant list and display them to user
     def list_ingrediants(self):
         for row in range(len(self.ingrediant_row)):
             print(f'{row+1}: {self.ingrediant_row[row][0]} - {self.ingrediant_row[row][1]} {self.ingrediant_row[row][2]}')
 
+    # add new ingrediant to list
     def input_ingrediant(self):
         print('== Input details of new ingrediant ==')
         # TODO add data validation
@@ -16,6 +18,7 @@ class RecipeIngrediants:
         ingrediant = [new_ingrediant, new_value, new_size]
         self.ingrediant_row.append(ingrediant)
 
+    # change an ingrediant that already exists
     def change_ingrediant(self, choice):
         choice = choice-1
         print('Current ingrediant:')
--- recipe_instructions.py
+++ recipe_instructions.py
@@ -1,4 +1,10 @@
 class RecipeInstructions:
 
     def __init__(self):
+        self.instructions = []
+
+    def list_instructions(self):
+        pass
+
+    def add_instructions(self):
         pass
